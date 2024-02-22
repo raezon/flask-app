87e4689ae935142f2837ae98bb33b6d02ce7f1c5
ammar djebabla <47975444+raezon@users.noreply.github.com>
2024-02-22 22:37:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'message': 'Hello, World!'})
+    return jsonify({'message': 'Hello, Devops!'})
 
 @app.route('/api/info')
 def info():
