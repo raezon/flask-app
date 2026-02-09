34cc6e13c69b38365196e342318bbe1865e7ebce
ammar djebabla <47975444+raezon@users.noreply.github.com>
2026-02-09 15:53:28 +0100

    Update greeting message in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'message': 'Hello, Devops 2042!'})
+    return jsonify({'message': 'Hello, Devops 2045!'})
 
 @app.route('/api/info')
 def info():
